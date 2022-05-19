fedb09b11b0aef66ded73dbb21edd900de21e0bb
EvgenyDavydenko <frontend82@mail.ru>
2022-05-19 21:32:22 +0300

    add Twig to project & render 2 pages

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+$twig = new Environment($loader);
 
 // Instantiate app
 $app = AppFactory::create();
@@ -12,14 +17,16 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, false, false);
 
 // Add route callbacks
-$app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write('Hello World');
+$app->get('/', function (Request $request, Response $response, array $args) use ($twig) {
+    $body = $twig->render('home.twig.html');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/{name}', function (Request $request, Response $response, array $args) {
+$app->get('/{name}', function (Request $request, Response $response, array $args) use ($twig) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+    $body = $twig->render('hello.twig.html', ['name' => $name]);
+    $response->getBody()->write($body);
     return $response;
 });
 
